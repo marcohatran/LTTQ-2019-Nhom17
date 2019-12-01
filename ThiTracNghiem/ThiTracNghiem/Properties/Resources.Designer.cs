3f8d2fe8f5c2a151ee05ea1a233260ff1efb14fe
nguyennguyenloc <48286184+nguyennguyenloc@users.noreply.github.com>
2019-12-01 22:36:52 +0700

    Login
    Login Form

--- ThiTracNghiem/ThiTracNghiem/Properties/Resources.Designer.cs
+++ ThiTracNghiem/ThiTracNghiem/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace THITRACNGHIEM.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap login {
+            get {
+                object obj = ResourceManager.GetObject("login", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap logout {
             get {
                 object obj = ResourceManager.GetObject("logout", resourceCulture);
